b03c9ed16d0e5e95ef2176fa950febca9fe66c15
awwa500@gmail.com <awwa500@gmail.com>
2014-06-01 18:12:23 +0900

    Add encode option to build.gradle

--- src/main/java/com/github/sendgridjp/SendGridJavaExample.java
+++ src/main/java/com/github/sendgridjp/SendGridJavaExample.java
@@ -2,47 +2,37 @@ package com.github.sendgridjp;
 
 import java.lang.*;
 import java.io.File;
-import com.github.sendgrid.SendGrid;
-import com.sendgrid.smtpapi.SMTPAPI;
 import java.io.FileNotFoundException;
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
 
 public class SendGridJavaExample {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException, SendGridException {
     String sendgridUsername  = System.getenv("SENDGRID_USERNAME");
     String sendgridPassword  = System.getenv("SENDGRID_PASSWORD");
-    String[] tos              = System.getenv("TOS").split(",",0);
-    String from               = System.getenv("FROM");
+    String[] tos             = System.getenv("TOS").split(",",0);
+    String from              = System.getenv("FROM");
 
-    SMTPAPI smtpapi = new SMTPAPI();
-    smtpapi.setTos(tos);
-    String[] fullnames = { "田中 太郎", "佐藤 次郎", "鈴木 三郎" };
-    for (String fullname : fullnames) {
-        smtpapi.addSubstitution("fullname", fullname);
+    SendGrid.Email email = new SendGrid.Email();
+    for (String to : tos) {
+        email.addTo(to);
     }
-    String[] familynames = { "田中", "佐藤", "鈴木" };
-    for (String familyname : familynames) {
-        smtpapi.addSubstitution("familyname", familyname);
-    }
-    String[] places = { "office", "home", "office" };
-    for (String place : places) {
-        smtpapi.addSubstitution("place", place);
-    }
-    smtpapi.addSection("office", "中野");
-    smtpapi.addSection("home", "目黒");
-    smtpapi.addCategory("カテゴリ1");
+    email.addSubstitution("fullname", new String[] { "田中 太郎", "佐藤 次郎", "鈴木 三郎" });
+    email.addSubstitution("familyname", new String[] { "田中", "佐藤", "鈴木" });
+    email.addSubstitution("place", new String[] { "office", "home", "office" });
+    email.addSection("office", "中野");
+    email.addSection("home", "目黒");
+    email.addCategory("カテゴリ1");
+    email.setFrom(from);
+    email.setFromName("送信者名");
+    email.setSubject("[sendgrid-java-example] フクロウのお名前はfullnameさん");
+    email.setText("familyname さんは何をしていますか？\r\n 彼はplaceにいます。");
+    email.setHtml("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。");
+    File file = new File("./gif.gif");
+    email.addAttachment(file, "owl.gif");
 
     SendGrid sendgrid = new SendGrid(sendgridUsername, sendgridPassword);
-    sendgrid.addTo(from);	// SMTPAPIのsetTos()を使用しているため、実際にはこのアドレスにはメールは送信されない
-    sendgrid.setFrom(from);
-    sendgrid.setFromName("送信者名");
-    sendgrid.setSubject("[sendgrid-java-example] フクロウのお名前はfullnameさん");
-    sendgrid.setText("familyname さんは何をしていますか？\r\n 彼はplaceにいます。");
-    sendgrid.setHtml("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。");
-    sendgrid.addHeader("x-smtpapi", smtpapi.jsonString());
-    sendgrid.setReplyTo("no-reply@owl.com");
-    sendgrid.addFile(new File("./gif.gif"));
-
-    String response = sendgrid.send();
-    System.out.println(response);
+    SendGrid.Response response = sendgrid.send(email);
+    System.out.println(response.getMessage());
   }
 }
