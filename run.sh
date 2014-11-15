f4d31b6bcab6ca90934f2d0613875ce9e58867fc
awwa500@gmail.com <awwa500@gmail.com>
2014-11-16 08:03:04 +0900

    update for sendgrid 1.3.0

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ for line in `cat "$filename"`
 do
 	export $line
 done
-java -cp repo/com/github/sendgrid-java-example/0.0.1/sendgrid-java-example-0.0.1-jar.jar com.github.sendgridjp.SendGridJavaExample
-
+java -cp repo/com/github/sendgrid-java-example/0.0.2/sendgrid-java-example-0.0.2-jar.jar com.github.sendgridjp.SendGridJavaExample
